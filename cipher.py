7c8956a2b7b07af3fb614858aea3f7c8d3d70811
Hem Paudel <sagar709@hotmail.com>
2024-11-04 21:11:02 -0500

    Caesar Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
 # add your code here
+def caesar_cipher(text, shift=5):
+    encrypted_text = 
+    
+    for char in text:
+        if char.isalpha():
+            offset = ord('A') if char.isupper() else ord('a')
+            encrypted_char = chr((ord(char) - offset + shift) % 26 + offset)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+            
+    return encrypted_text
+
+plain_text = input(Please enter a sentence: )
+encrypted_text = caesar_cipher(plain_text)
+print(The encrypted sentence is:, encrypted_text)
